cf53035b8ccac9ea32ed86409706146675ccece3
Rodrigo Alencar <rodrigo.alencar@conductor.com.br>
2019-06-05 17:35:12 -0300

    Create [ProductWithIngredients] on domain model

---
+++ GeekBurger.Ingredients.DomainModel/ProductWithIngredients.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurger.Ingredients.DomainModel
+{
+    public class ProductWithIngredients
+    {
+        public string ProductId { get; set; }
+
+        public string StoreId { get; set; }
+
+        public ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
