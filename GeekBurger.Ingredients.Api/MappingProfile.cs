136dbe652761a5a0e94de50fbd3eeb9b98deb46d
Rodrigo Alencar <rodrigo.alencar@conductor.com.br>
2019-06-05 17:35:30 -0300

    Update mapping profile for automapper

--- GeekBurger.Ingredients.Api/MappingProfile.cs
+++ GeekBurger.Ingredients.Api/MappingProfile.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using GeekBurger.Ingredients.DomainModel;
+using GeekBurger.Products.Contract;
 
 namespace GeekBurger.Ingredients.Api
 {
@@ -10,6 +11,15 @@ namespace GeekBurger.Ingredients.Api
             this.CreateMap<LabelImageAddedMessage, Ingredient>()
                 .ForMember(destiny => destiny.Name, src => src.MapFrom(l => l.ItemName))
                 .ForMember(destiny => destiny.Composition, src => src.MapFrom(l => l.Ingredients))
+                .ReverseMap();
+
+            this.CreateMap<ItemToGet, Ingredient>()
+                .ForMember(destiny => destiny.Name, src => src.MapFrom(itg => itg.Name));
+
+            this.CreateMap<ProductChangedMessage, ProductWithIngredients>()
+                .ForMember(destiny => destiny.StoreId, src=> src.MapFrom(pcm => pcm.Product.StoreId))
+                .ForMember(destiny => destiny.ProductId, src => src.MapFrom(pcm => pcm.Product.ProductId))
+                .ForMember(destiny => destiny.Ingredients, src => src.MapFrom(pcm => pcm.Product.Items))
                 .ReverseMap();
         }
     }
